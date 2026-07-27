cfc4ffaa19d6a5806bda5bea7e3f5a3384bd1ae4
Dhruv Yadav <dhruvyadavtech@gmail.com>
2026-07-27 19:45:25 +0530

    Time: 581 ms (54.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT ROUND(SUM(player_login)/COUNT(DISTINCT player_id),2) as fraction
+FROM (
+SELECT player_id, DATEDIFF(event_date,min(event_date) OVER(PARTITION BY player_id))=1 AS player_login
+FROM Activity) as t
