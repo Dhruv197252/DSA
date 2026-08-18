d15c7f5ba5ce05c68b16c507e1ede7c3c9b359a5
Dhruv Yadav <dhruvyadavtech@gmail.com>
2026-08-18 20:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (9.64%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     
-    int Operate(int a, int b, string token){
-        if(token=="+"){
-            return a+b;
-        }
-        if(token=="-"){
-            return a-b;
-        }
-        if(token=="*"){
-            return a*b;
-        }
-        if(token=="/"){
-            return a/b;
-        }
-        return -1;
-    }
+    // int Operate(int a, int b, string token){
+    //     if(token=="+"){
+    //         return a+b;
+    //     }
+    //     if(token=="-"){
+    //         return a-b;
+    //     }
+    //     if(token=="*"){
+    //         return a*b;
+    //     }
+    //     if(token=="/"){
+    //         return a/b;
+    //     }
+    //     return -1;
+    // }
 
 
     int evalRPN(vector<string>& tokens) {
@@ -23,6 +23,17 @@ public:
         // for(int i=0;i<tokens.size();i++){
         //     string & token = tokens[i];
         // }
+
+        unordered_map<string,function<int(int, int)>>mp={
+            {"+",[](int a,int b){return a+b;}},
+            {"-",[](int a,int b){return a-b;}},
+            {"*",[](int a,int b){return a*b;}},
+            {"/",[](int a,int b){return a/b;}}
+
+        };
+
+
+
         for(string &token : tokens){
             if(token=="+"||token=="-"||token=="*"||token =="/"){
                 int b = st.top();
@@ -30,7 +41,7 @@ public:
                 int a = st.top();
                 st.pop();
 
-                int result = Operate(a,b,token);
+                int result = mp[token](a,b);
                 st.push(result);
             }else{
                 st.push(stoi(token));
